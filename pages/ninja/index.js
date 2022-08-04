626a668fd0467473dba438920a82cf5fbb15c0c7
NOVA <anna_tandem@mail.ru>
2022-08-04 11:30:50 +0200

    Added Ninjas

--- pages/ninja/index.js
+++ pages/ninja/index.js
@@ -1,8 +1,35 @@
-const Ninja = () => {
+import React from "react";
+import Head from "next/head";
+import styles from '../../styles/Ninja.module.css';
+import Link from 'next/link';
+
+export const getStaticProps = async() => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+
+    return  {
+        props: {ninjaProfiles: data}
+    }
+}
+
+const Ninja = ({ ninjaProfiles }) => {
     return ( 
-        <div>
-            <h1>Ninjas here!</h1>
-        </div>
+        <>
+            <Head>
+                <title>Ninja | Ningas</title>
+                <meta name='keywords' content='ninja'/>
+            </Head>
+            <div>
+                <h1>Ninjas here!</h1>
+                {ninjaProfiles.map(profile => (
+                    <Link href={'./ninja/' + profile.id} key={profile.id}>
+                        <a className={styles.single}>
+                            <h3>{profile.name}</h3>
+                        </a>
+                    </Link>
+                ))} 
+            </div>
+        </>
      );
 }
  
